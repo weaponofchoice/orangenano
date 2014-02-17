ad21691c0f63276dd65b49d78dbc6edbd4f33cd7
LucAwater <lucawater@gmail.com>
2014-02-17 18:10:28 +0100

    Revert "footer tweaks"
    This reverts commit 3a9feaf768d0011c6ad512b79e4ad3cec930cb3c.

--- content/themes/orangenano/coffee/accordion.coffee
+++
@@ -1,9 +0,0 @@
-if $('.accordion').length > 0
-	target= $('.accordion')
-	
-	target.accordion({
-		header: 'h4', 
-		animate: 200, 
-		collapsible: true, 
-		active: false,
-	});
---
+++ content/themes/orangenano/js/collapse.js
@@ -0,0 +1,16 @@
+$(function() {
+  var icons = {
+    header: "ui-icon-circle-arrow-e",
+    activeHeader: "ui-icon-circle-arrow-s"
+  };
+  $( "#accordion" ).accordion({
+    icons: icons
+  });
+  $( "#toggle" ).button().click(function() {
+    if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+      $( "#accordion" ).accordion( "option", "icons", null );
+    } else {
+      $( "#accordion" ).accordion( "option", "icons", icons );
+    }
+  });
+});
