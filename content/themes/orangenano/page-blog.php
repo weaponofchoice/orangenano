a8ea25267932e728d4a365d5b0972467999df346
Luc Awater <lucawater@gmail.com>
2014-04-24 12:52:16 +0200

    added page-blog with link to home.php

---
+++ content/themes/orangenano/page-blog.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+	<?php if( have_posts() ): ?>
+		<?php while( have_posts() ): the_post(); ?>
+			
+			<?php include( locate_template('home.php') ); ?>
+			
+		<?php endwhile; ?>
+	<?php endif; ?>
+	
+<?php else : ?>
+	<h3 class="center">Not found</h3>
+	<p class="center">Sorry, but you are looking for something that isn't here.</p>
+<?php endif; wp_reset_postdata(); ?>
+
+<?php get_footer(); ?>
