90aaf63710d2e75ea9cc72f11882adc3965d6fec
Luc Awater <lucawater@gmail.com>
2014-04-29 14:04:49 +0200

    fixed double video post

--- content/themes/orangenano/home.php
+++ content/themes/orangenano/home.php
@@ -7,65 +7,52 @@
 		</div>
 	</div>
 	
-	<?php if( has_post_video() ): ?>
-		
-		<div class="post post_video block">
-			<p class="medium-12 columns italic"><?php the_time('F j, Y'); ?></p>
-			<div class="post_content medium-4 columns">
-				<!-- post title -->
-				<h2 class="medium-12 columns"><?php the_title(); ?></h2>
-				
-				<!-- summary of text -->
-				<?php the_excerpt(); ?><!-- define classes in functions.php -->
-				
-				<!-- 'read more' link -->
-				<a class="columns readmore bold italic" href="<?php the_permalink(); ?>">read more</a>
-			</div>
-			<div class="image_holder medium-8 columns"><?php the_post_thumbnail(); ?></div>
-		</div>
-		
-	<?php endif; ?>
-	
-	<?php if( have_posts() ): ?> 
+	<?php if ( have_posts() ): ?>
 		<?php while( have_posts() ): the_post(); ?>
-		
-		<div class="post block">
-			<p class="medium-12 columns italic"><?php the_time('F j, Y'); ?></p>
-			<div class="image_holder medium-3 columns"><?php the_post_thumbnail(); ?></div>
-			<div class="post_content medium-9 columns">
-				<!-- post title -->
-				<h2 class="medium-8 columns"><?php the_title(); ?></h2>
+			
+			<?php if( has_post_video() ): ?>
 				
-				<!-- author profile picture -->
-				<div class="medium-1 medium-offset-1 columns"><?php the_author_image(); ?></div>
+				<div class="post post_video block">
+					<p class="medium-12 columns italic"><?php the_time('F j, Y'); ?></p>
+					<div class="post_content medium-4 columns">
+						<!-- post title -->
+						<h2 class="medium-12 columns"><?php the_title(); ?></h2>
 				
-				<!-- author name -->
-				<div class="author medium-2 columns">
-					<p>author:</p>
-					<p class="bold"><?php the_author(); ?></p>
+						<!-- summary of text -->
+						<?php the_excerpt(); ?><!-- define classes in functions.php -->
+					</div>
+					<div class="image_holder medium-8 columns"><?php the_post_thumbnail(); ?></div>
 				</div>
 				
-				<!-- summary of text -->
-				<?php the_excerpt(); ?><!-- define classes in functions.php -->
+			<?php else : ?>
 				
-				<!-- 'read more' link -->
-				<a class="columns readmore bold italic" href="<?php the_permalink(); ?>">read more</a>
-			</div>
-		</div>
-	
-	<?php endwhile; ?>
-	<div class="pagination">
-		<?php
-		global $wp_query;
-
-		echo paginate_links( array(
-			'format' => '/blog/page/%#%',
-			'end_size' => 10,
-			'current' => 0,		
-			'total' => $wp_query->max_num_pages
-		) );
-		?>
-	</div>
+				<div class="post block">
+					<p class="medium-12 columns italic"><?php the_time('F j, Y'); ?></p>
+					<div class="image_holder medium-3 columns"><?php the_post_thumbnail(); ?></div>
+					<div class="post_content medium-9 columns">
+						<!-- post title -->
+						<h2 class="medium-8 columns"><?php the_title(); ?></h2>
+				
+						<!-- author profile picture -->
+						<div class="medium-1 medium-offset-1 columns"><?php the_author_image(); ?></div>
+				
+						<!-- author name -->
+						<div class="author medium-2 columns">
+							<p>author:</p>
+							<p class="bold"><?php the_author(); ?></p>
+						</div>
+				
+						<!-- summary of text -->
+						<?php the_excerpt(); ?><!-- define classes in functions.php -->
+				
+						<!-- 'read more' link -->
+						<a class="columns readmore bold italic" href="<?php the_permalink(); ?>">read more</a>
+					</div>
+				</div>
+				
+			<?php endif; ?>
+		
+		<?php endwhile; ?>
 	
 <?php else : ?>
 	<h3 class="center">Not found</h3>
