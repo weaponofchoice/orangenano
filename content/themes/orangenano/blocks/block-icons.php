ec736907b0d48c1e53f035ccfdf24f2862040b34
Luc Awater <lucawater@gmail.com>
2014-06-12 15:59:17 +0200

    added ON link to icons block

--- content/themes/orangenano/blocks/block-icons.php
+++ content/themes/orangenano/blocks/block-icons.php
@@ -32,6 +32,18 @@
 					
 			<?php endif; ?>
 			</ul>
+			
+			<?php if( have_rows('link_to_ON') ): ?>
+				<?php while( have_rows('link_to_ON') ): the_row(); ?>
+					<a href="<?php the_sub_field('link'); ?>">
+						<div class="link_on small-10">
+							<h2><?php the_sub_field('title')?></h2>
+							<div class="arrow right"><span></span></div>
+							<p><?php the_sub_field('subtitle'); ?></p>
+						</div>
+					</a>
+				<?php endwhile; ?>
+			<?php endif; ?>
 		</article>
 	</div>
 </div>
